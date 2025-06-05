3ee6fde3ecbc2e64c99c9fc9f658c63060af38ad
Benji Sloan <sloanrobe@hotmail.com>
2025-06-05 14:43:46 +0200

    Added section for work experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div className='experience'>
+      <img src="public\images\PPCS-logo.jpeg" alt="Pembroke Pines Charter School" />
+      <img src="public\images\ahs.jpg" alt="American Heritage School - Plantation" />
+      <img src="public\images\adventure-bay-learning.png" alt="Adventure Bay Learning Center" />
+        
+    </div>
+  )
+}
+
+export default Experience
